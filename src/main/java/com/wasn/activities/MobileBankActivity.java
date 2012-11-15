0a343f2e514ad74ece518556be9c0896408ef320
erangah <erangah@pagero.com>
2012-11-15 19:36:18 +0530

    Modified mobile bank activity

--- src/main/java/com/wasn/activities/MobileBankActivity.java
+++ src/main/java/com/wasn/activities/MobileBankActivity.java
@@ -2,9 +2,13 @@ package com.wasn.activities;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import com.wasn.application.MobileBankApplication;
 
 /**
  * Main activity class of the application
@@ -13,9 +17,14 @@ import android.widget.Button;
  */
 public class MobileBankActivity extends Activity implements View.OnClickListener {
 
+    MobileBankApplication application;
+
+    // activity components
     Button transactionButton;
     Button summaryButton;
     Button settingsButton;
+    RelativeLayout logout;
+    TextView logoutText;
 
     /**
      * {@inheritDoc}
@@ -25,13 +34,30 @@ public class MobileBankActivity extends Activity implements View.OnClickListener
         super.onCreate(savedInstanceState);
         setContentView(R.layout.mobile_bank_layout);
 
+        init();
+    }
+
+    /**
+     * Initialize activity components
+     */
+    public void init() {
+        application = (MobileBankApplication) MobileBankActivity.this.getApplication();
+
         transactionButton = (Button) findViewById(R.id.mobile_bank_layout_transaction_button);
         summaryButton = (Button) findViewById(R.id.mobile_bank_layout_summary_button);
         settingsButton = (Button) findViewById(R.id.mobile_bank_layout_settings_button);
+        logout = (RelativeLayout) findViewById(R.id.mobile_bank_layout_logout);
+
+        // set custom font
+        logoutText = (TextView) findViewById(R.id.mobile_bank_layout_logout_text);
+        Typeface face= Typeface.createFromAsset(getAssets(), "fonts/vegur_2.otf");
+        logoutText.setTypeface(face);
+        //logoutText.setTypeface(null, Typeface.BOLD);
 
         transactionButton.setOnClickListener(MobileBankActivity.this);
         summaryButton.setOnClickListener(MobileBankActivity.this);
         settingsButton.setOnClickListener(MobileBankActivity.this);
+        logout.setOnClickListener(MobileBankActivity.this);
     }
 
     /**
@@ -50,6 +76,11 @@ public class MobileBankActivity extends Activity implements View.OnClickListener
             // display transaction activity
             startActivity(new Intent(MobileBankActivity.this, SettingsActivity.class));
             MobileBankActivity.this.finish();
+        } else if(view == logout) {
+            // logout
+            startActivity(new Intent(MobileBankActivity.this, LoginActivity.class));
+            MobileBankActivity.this.finish();
+            application.resetFields();
         }
     }
 
