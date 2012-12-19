acff72db2e74b05fd3aa9beae1d71d9f4736995f
erangah <erangah@pagero.com>
2012-12-19 12:10:49 +0530

    Added admin login dialog to mobile bank main activity

--- src/main/java/com/wasn/activities/MobileBankActivity.java
+++ src/main/java/com/wasn/activities/MobileBankActivity.java
@@ -8,10 +8,10 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
-import android.widget.Button;
-import android.widget.RelativeLayout;
-import android.widget.TextView;
+import android.widget.*;
 import com.wasn.application.MobileBankApplication;
+import com.wasn.exceptions.UnAuthenticatedUserException;
+import com.wasn.utils.LoginUtils;
 
 /**
  * Main activity class of the application
@@ -81,9 +81,8 @@ public class MobileBankActivity extends Activity implements View.OnClickListener
             startActivity(new Intent(MobileBankActivity.this, TransactionListActivity.class));
             MobileBankActivity.this.finish();
         } else if(view == settingsButton) {
-            // display transaction activity
-            startActivity(new Intent(MobileBankActivity.this, SettingsActivity.class));
-            MobileBankActivity.this.finish();
+            // admin privileges need to view or edit settings
+            displayAdminLoginDialog();
         } else if(view == logout) {
             displayInformationMessageDialog("Are you sure, you want to logout? ");
         }
@@ -140,6 +139,70 @@ public class MobileBankActivity extends Activity implements View.OnClickListener
         });
 
         dialog.show();
+    }
+
+    public void displayAdminLoginDialog() {
+        final Dialog dialog = new Dialog(MobileBankActivity.this);
+
+        //set layout for dialog
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setContentView(R.layout.admin_login_dialog_layout);
+        dialog.getWindow().setLayout(ViewGroup.LayoutParams.FILL_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        dialog.setCancelable(true);
+
+        // set custom font
+        TextView messageHeaderTextView = (TextView) dialog.findViewById(R.id.admin_login_layout_message_header_text);
+        TextView messageTextView = (TextView) dialog.findViewById(R.id.admin_login_layout_message_text);
+        Typeface face= Typeface.createFromAsset(getAssets(), "fonts/vegur_2.otf");
+        messageHeaderTextView.setTypeface(face);
+        messageHeaderTextView.setTypeface(null, Typeface.BOLD);
+        messageTextView.setTypeface(face);
+
+        //set ok button
+        Button okButton = (Button) dialog.findViewById(R.id.admin_login_layout_ok_button);
+        okButton.setTypeface(face);
+        okButton.setTypeface(null, Typeface.BOLD);
+        okButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // validate password here
+                try {
+                    EditText passwordEditText = (EditText) dialog.findViewById(R.id.admin_login_layout_password);
+                    String password = passwordEditText.getText().toString().trim();
+                    LoginUtils.validateAdminPassword(password);
+
+                    // display settings activity
+                    startActivity(new Intent(MobileBankActivity.this, SettingsActivity.class));
+                    MobileBankActivity.this.finish();
+                    dialog.cancel();
+                } catch (UnAuthenticatedUserException e) {
+                    e.printStackTrace();
+                    displayToast("Invalid password");
+                } catch (IllegalArgumentException e) {
+                    e.printStackTrace();
+                    displayToast("Password empty");
+                }
+            }
+        });
+
+        // cancel button
+        Button cancelButton = (Button) dialog.findViewById(R.id.admin_login_layout_cancel_button);
+        cancelButton.setTypeface(face);
+        cancelButton.setTypeface(null, Typeface.BOLD);
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                dialog.cancel();
+            }
+        });
+
+        dialog.show();
+    }
+
+    /**
+     * Display toast message
+     * @param message message tobe display
+     */
+    public void displayToast(String message) {
+        Toast.makeText(MobileBankActivity.this, message, Toast.LENGTH_LONG).show();
     }
 
     /**
--- src/main/java/com/wasn/utils/LoginUtils.java
+++ src/main/java/com/wasn/utils/LoginUtils.java
@@ -1,5 +1,6 @@
 package com.wasn.utils;
 
+import com.wasn.exceptions.UnAuthenticatedUserException;
 import com.wasn.pojos.User;
 
 /**
@@ -14,10 +15,28 @@ public class LoginUtils {
      *
      * @param user current user
      */
-    public static void validateFields(User user) throws IllegalArgumentException, NumberFormatException {
+    public static void validateFields(User user) throws IllegalArgumentException {
         // check empty of fields
         if (user.getUsername().equals("") || user.getPassword().equals("")) {
             throw new IllegalArgumentException();
+        }
+    }
+
+    /**
+     * Validate administrative password
+     * @param password admin password
+     * @return validity of password
+     */
+    public static boolean validateAdminPassword(String password) throws IllegalArgumentException, UnAuthenticatedUserException {
+        // check empty
+        if(password.equals("")) {
+            throw new IllegalArgumentException();
+        } else {
+            if(password.equals("1234")) {
+                return true;
+            } else {
+                throw new UnAuthenticatedUserException();
+            }
         }
     }
 
